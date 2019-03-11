fbc4b1b002b65a180096faf9eb8abe331c92f439
beatriz <biiah_beatriz@hotmail.com>
2019-03-11 00:23:44 -0300

    Continuação

--- JavaScripts.js
+++ JavaScripts.js
@@ -1,28 +1,10 @@
-const selector= selector => document.querySelector(selector);
-const selectorAll= selector => document.querySelectorAll(selector);
+const selector = selector => document.querySelector(selector);
+const selectorAll = selector => document.querySelectorAll(selector);
 
 const hamburguer = selector('.menu-hamburguer');
-const hamburguerNav= selector('.nav-menu-hamburguer');
+const hamburguerNav = selector('.nav-menu-hamburguer');
 
-hamburguer.onclick= function() {
+hamburguer.onclick = function () {
     hamburguer.classList.toggle('ativo');
     hamburguerNav.classList.toggle('visible');
-}
-
-const selector= selector => document.querySelector(selector);
-const selectorAll= selector => document.querySelectorAll(selector);
-
-const cadastrar = selector('.botao');
-
-cadastrar.onclick = function(){
-    cadastrar.classList.toggle('ativo');
-}
-
-const selector= selector => document.querySelector(selector);
-const selectorAll= selector => document.querySelectorAll(selector);
-
-const div= selector('.emp-flex');
-
-empresa.onclick=function(){
-    empresa.classList.toggle('ativo');
 }
